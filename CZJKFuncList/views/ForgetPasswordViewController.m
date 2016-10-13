7d4bea8f5483521548e55ce03497d4f1e73f8e52
qf <qf@2379591029.vpn.umich.net>
2016-10-14 00:17:16 +0800

    1.0

--- CZJKFuncList/views/ForgetPasswordViewController.m
+++ CZJKFuncList/views/ForgetPasswordViewController.m
@@ -9,6 +9,7 @@
 #import "ForgetPasswordViewController.h"
 
 @interface ForgetPasswordViewController ()
+@property(nonatomic,strong)UITextField* username;
 
 @end
 
@@ -17,6 +18,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.username = [[UITextField alloc] initWithFrame:CGRectMake(10, 10, 300, 40)];
+    self.username.textColor = [UIColor blackColor];
+    self.username.placeholder = @"Username";
+    [self.view addSubview:self.username];
+    
+    UIButton* btn = [[UIButton alloc] initWithFrame:CGRectMake((CGRectGetWidth(self.view.frame)-200)/2.0, 110, 200, 30)];
+    [btn setTitle:NSLocalizedString(@"Get back password", nil) forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [btn setBackgroundColor:[UIColor darkGrayColor]];
+    [btn addTarget:self action:@selector(onClickBtn:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,5 +45,33 @@
     // Pass the selected object to the new view controller.
 }
 */
+-(void)onClickBtn:(id)sender{
+    if (![SXRSDKUtils validateEmail:self.username.text]) {
+        UIAlertController* ac = [UIAlertController alertControllerWithTitle:nil message:NSLocalizedString(@"Username is not valid Email", nil) preferredStyle:UIAlertControllerStyleAlert];
+        [ac addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+            
+        }]];
+        [self presentViewController:ac animated:YES completion:nil];
+        return;
+        
+    }
+    [[SXR shareInstance] SXRSDKForgetPasswd:self.username.text language:@"chs" callBack:^(NSDictionary *result, NSError *error) {
+        if (error) {
+            UIAlertController* ac = [UIAlertController alertControllerWithTitle:nil message:[NSString stringWithFormat:@"%@\nerror=%@",NSLocalizedString(@"Forget password Error", nil),error] preferredStyle:UIAlertControllerStyleAlert];
+            [ac addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+                
+            }]];
+            [self presentViewController:ac animated:YES completion:nil];
+            
+        }else{
+            UIAlertController* ac = [UIAlertController alertControllerWithTitle:nil message:[NSString stringWithFormat:@"%@\nresult=%@",NSLocalizedString(@"Send Forget Password OK", nil),result] preferredStyle:UIAlertControllerStyleAlert];
+            [ac addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+                
+            }]];
+            [self presentViewController:ac animated:YES completion:nil];
+            
+        }
+    }];
+}
 
 @end
